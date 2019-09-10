c34d243159a36deff78d890c0a2bd81b3509f018
Zoe Nolan <github@zoenolan.org>
2019-09-10 23:58:55 +0100

    Add extra valid checks

--- contracts/scarce-edition.sol
+++ contracts/scarce-edition.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721Mintable.sol";
 
-contract ScareEdition is ERC721, ERC721Full, ERC721Mintable {
+contract ScareEdition is ERC721Full, ERC721Mintable {
   constructor() ERC721Full("zien scarce editions", "AZSC") public {
   }
 
@@ -15,10 +15,12 @@ contract ScareEdition is ERC721, ERC721Full, ERC721Mintable {
     * @param workId uint64 ID of the work
     * @param editionNumber uint64 number of this edition
     * @param editionCount uint64 total count for this work
-    * @param uri string of the work
+    * @param uri uri of the work
     */
   function create(address to, uint64 artistId, uint64 workId, uint64 editionNumber, uint64 editionCount, string memory uri) public onlyMinter returns (bool) {
+    require(editionCount > 0);
     require(editionCount > editionNumber);
+
     uint256 tokenId = (artistId << 192) + (workId << 128) + (editionNumber << 64) + editionCount;
 
     _mint(to, tokenId);
