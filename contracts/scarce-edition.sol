6ec3ec003adedf70b8735a2daaa304a978254ff3
Zoe Nolan <github@zoenolan.org>
2019-09-10 23:13:03 +0100

    Add a function to create a edition

--- contracts/scarce-edition.sol
+++ contracts/scarce-edition.sol
@@ -6,4 +6,23 @@ import "@openzeppelin/contracts/token/ERC721/ERC721Mintable.sol";
 contract MyNFT is ERC721Full, ERC721Mintable {
   constructor() ERC721Full("Scarce Edition", "AZSC") public {
   }
+
+  /**
+    * @dev function to safely create a new token.
+    * Reverts if the given editionNumber is greater than editionCount.
+    * @param to The address that will own the minted token
+    * @param artistId uint64 ID of the artist who created the work
+    * @param workId uint64 ID of the work
+    * @param editionNumber uint64 number of this edition
+    * @param editionCount uint64 total count for this work
+    */
+  function create(address to, uint64 artistId, uint64 workId, uint64 editionNumber, uint64 editionCount) public onlyMinter returns (bool) {
+    require(editionCount > editionNumber);
+    uint256 tokenId = (artistId << 192) + (workId << 128) + (editionNumber << 64) + editionCount;
+
+    _mint(to, tokenId);
+
+    return true;
+  }
+
 }
