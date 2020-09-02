a4fc67bae2cb8aaa782b4c3e1ba320d8a700f4d4
Zoe Nolan <github@zoenolan.org>
2020-09-02 23:43:38 +0100

    Move to the new version of OpenZeppelin

--- contracts/scarce-edition.sol
+++ contracts/scarce-edition.sol
@@ -1,29 +1,19 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.6.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721Mintable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract ScareEdition is ERC721Full, ERC721Mintable {
-  constructor() ERC721Full("zien scarce editions", "AZSC") public {
-    _addMinter(msg.sender);
+contract ScareEdition is ERC721 {
+  constructor() ERC721("zien scarce editions", "AZSC") public {
   }
 
   /**
     * @dev function to safely create a new token.
     * Reverts if the given editionNumber is greater than editionCount.
     * @param to The address that will own the minted token
-    * @param artistId uint64 ID of the artist who created the work
-    * @param workId uint64 ID of the work
-    * @param editionNumber uint64 number of this edition
-    * @param editionCount uint64 total count for this work
+    * @param tokenId, uint256 ID of the work
     * @param uri uri of the work
     */
-  function create(address to, uint64 artistId, uint64 workId, uint64 editionNumber, uint64 editionCount, string memory uri) public onlyMinter returns (bool) {
-    require(editionCount > 0);
-    require(editionCount > editionNumber);
-
-    uint256 tokenId = (artistId << 192) + (workId << 128) + (editionNumber << 64) + editionCount;
-
+  function create(address to, uint256 tokenId, string memory uri) public returns (bool) {
     _mint(to, tokenId);
     _setTokenURI(tokenId, uri); 
 
