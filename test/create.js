8900e4da24a61b80b3598b31b586b698169bf093
Zoe Nolan <github@zoenolan.org>
2020-09-07 00:13:17 +0100

    Add a test for creating a scarce edition when not the contract owner

--- test/create.js
+++ test/create.js
@@ -25,4 +25,24 @@ contract('ScarceEditionCreate', (accounts) => {
  	assert.strictEqual(totalSupplyBefore.toNumber() + 1, totalSupplyAfter.toNumber());
         assert.strictEqual(tokenURIBefore, tokenURIAfter);
     });
+    
+    it('create: Create a new Edition when not the contract owner', async () => {
+        const addrRecipient = accounts[1];
+        const tokenID = 1234567890
+        const tokenURIBefore = "http://scarce.editions/one"
+
+        const totalSupplyBefore = await CONTRACT.totalSupply.call();
+
+        let actualError = null;
+        try {
+              const createRes = await CONTRACT.create(addrRecipient, tokenID, tokenURIBefore, { from: addrRecipient });
+        } catch (error) {
+            actualError = error;
+        }
+
+        const totalSupplyAfter = await CONTRACT.totalSupply.call();
+
+        assert.strictEqual(totalSupplyBefore.toNumber(), totalSupplyAfter.toNumber());
+        assert.strictEqual(actualError.toString(),"Error: Returned error: VM Exception while processing transaction: revert Only the contract owner can perform this operation -- Reason given: Only the contract owner can perform this operation.");
+    });      
 });
