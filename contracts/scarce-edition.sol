b2d249c256f27ad678fbaa6fb1cbb62f9b3702ad
Zoe Nolan <github@zoenolan.org>
2020-09-06 21:59:26 +0100

    Add only owner mod

--- contracts/scarce-edition.sol
+++ contracts/scarce-edition.sol
@@ -1,10 +1,22 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract ScareEdition is ERC721 {
+  address private _owner;
+
   constructor() ERC721("zien scarce editions", "AZSC") public {
+    _owner = msg.sender;
   }
+  
+  modifier onlyOwner() {
+    if (msg.sender != _owner) {
+      revert("Only the contract owner can perform this operation");
+    }
+    _;
+  }  
 
   /**
     * @dev function to safely create a new token.
@@ -13,7 +25,7 @@ contract ScareEdition is ERC721 {
     * @param tokenId, uint256 ID of the work
     * @param uri uri of the work
     */
-  function create(address to, uint256 tokenId, string memory uri) public returns (bool) {
+  function create(address to, uint256 tokenId, string memory uri) public onlyOwner returns (bool) {
     _mint(to, tokenId);
     _setTokenURI(tokenId, uri); 
 
