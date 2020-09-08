a2d1267c972ecadde67558c0db52cd0d8ece7307
Zoe Nolan <github@zoenolan.org>
2020-09-08 20:55:06 +0100

    Add missing copyright header

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 const Migrations = artifacts.require("Migrations");
 
 module.exports = function(deployer) {
--- migrations/2_deploy_scarce_edition.js
+++ migrations/2_deploy_scarce_edition.js
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 const ScarceEdition = artifacts.require(`./ScarceEdition.sol`)
 
 module.exports = (deployer) => {
