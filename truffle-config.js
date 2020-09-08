794aa9ac6795b3647b2af72135c25fe159df6bd0
Zoe Nolan <github@zoenolan.org>
2020-09-08 20:37:09 +0100

    Add deployment config

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,9 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const mnemonic = "Your account mnemonic";
+const infuraKey = "Your Infura project ID";
 
 module.exports = {
   /**
@@ -46,40 +44,39 @@ module.exports = {
       host: "localhost",
       port: 7545,
       network_id: "*" // Match any network id
-    },  
-
-    // Another network with more advanced options...
-    // advanced: {
-      // port: 8777,             // Custom port
-      // network_id: 1342,       // Custom network
-      // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-      // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-      // from: <address>,        // Account to send txs from (default: accounts[0])
-      // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-
-    // Useful for private networks
-    // private: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-      // network_id: 2111,   // This network is yours, in the cloud.
-      // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
-  },
-
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
+    }, 
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/"+infuraKey);
+      },
+      network_id: 3,
+      gasPrice: 20000000000,
+      gas: 3716887
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/"+infuraKey);
+      },
+      network_id: 4,
+      gasPrice: 20000000000,
+      gas: 3716887
+    },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://kovan.infura.io/v3/"+infuraKey);
+      },
+      network_id: 42,
+      gasPrice: 20000000000,
+      gas: 3716887
+    },
+    main: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/"+infuraKey);
+      },
+      network_id: 1,
+      gasPrice: 20000000000, // 20 GWEI
+      gas: 3716887    // gas limit, set any number you want
+    }    
   },
 
   // Configure your compilers
