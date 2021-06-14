1035c00c66b8a7048f1a801775eab686a6244acd
Zoe Nolan <zoe@zien.io>
2021-06-14 22:17:01 +0100

    Contract compiling with Hardhat

--- contracts/ScarceEdition.sol
+++ contracts/ScarceEdition.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.0;
+pragma solidity ^0.6.2;
 pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
--- migrations/2_deploy_scarce_edition.js
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-const ScarceEdition = artifacts.require(`./ScarceEdition.sol`)
-
-module.exports = (deployer) => {
-  deployer.deploy(ScarceEdition);
-}
