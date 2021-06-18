a351a79c18edd3497c973e89bf83fc839329e1f8
Zoe Nolan <zoe@zien.io>
2021-06-18 22:00:03 +0100

    Add ABI generation

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,10 @@
 import '@nomiclabs/hardhat-waffle';
 import '@nomiclabs/hardhat-solhint';
-import "solidity-coverage"
-import "hardhat-gas-reporter"
+import "solidity-coverage";
+import "hardhat-gas-reporter";
+import "hardhat-abi-exporter";
 
-// import { HardhatUserConfig } from 'hardhat/types';
+import { HardhatUserConfig } from "hardhat/types";
 
 // Here you can write Hardhat tasks. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -11,6 +12,24 @@ import "hardhat-gas-reporter"
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-module.exports = {
-	solidity: '0.6.2',
-};
+
+const config: HardhatUserConfig = {
+	abiExporter: {
+		path: './abi',
+		clear: true,
+		flat: true,
+		only: [],
+		spacing: 2
+	},
+	solidity: {
+		version: "0.6.2",
+		settings: {
+		  optimizer: {
+			enabled: true,
+			runs: 200
+		  }
+		}
+	  },	
+  };
+
+export default config;
