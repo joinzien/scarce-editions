9063e0fc55e486bb2debd2bda3853b1cb9c95369
LDNCWRB <londoncodeworkswebdevprogramrb@outlook.com>
2021-07-20 12:27:57 +0100

    updates batchCreate test

--- test/sampleTests_BatchCreate.ts
+++ test/sampleTests_BatchCreate.ts
@@ -27,104 +27,40 @@ describe('ScarceEdition_BatchCreate', function () {
 
 	describe('Transactions', () => {
 
-		//
-
 		it('batchCreate: Create a new Edition as the contract owner', async () => {
-			const to_addresses = [
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-				addrRecipient.address,
-			];
 
-			const token_ids = [
-				1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1,
-				2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-			];
+			// uint256 batchSize
+			let batchSize = 10;
 
-			const urls = [
-				'http://url.io/drop/01/scarce-edition/211345a53c261a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c262a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c263a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c264a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c265a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c266a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c267a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c268a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c269a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c26aa7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c261a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c262a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c263a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c264a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c265a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c266a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c267a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c268a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c269a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c26aa7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c261a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c262a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c263a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c264a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c265a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c266a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c267a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c268a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c269a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c26aa7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c261a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c262a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c263a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c264a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c265a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c266a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c267a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c268a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c269a7e877b1e05a8f239e9',
-				'http://url.io/drop/01/scarce-edition/211345a53c26aa7e877b1e05a8f239e9',
-			];
+			// uint256 maxBatch 
+			let maxbatch = 40;
+
+			// address[maxBatch] memory to
+			let to_addresses = new Array();
+			for (let i = 1; i <= maxbatch; i++) {
+				to_addresses.push(addrRecipient.address);
+			}
+			
+			// uint256[maxBatch] memory tokenId
+			let token_ids = new Array();
+			let j = 1
+			for (let k = 1; k <= maxbatch; k++ ) {
+				if ( j > 10 ) { j = 1 }
+				token_ids.push(j);
+				j++
+			}
+
+			// string[maxBatch] memory uri
+			let urls = new Array();
+			for ( let k = 1; k <= maxbatch; k++) {
+				urls.push('http://url.io/drop/01/scarce-edition/211345a53c261a7e877b1e05a8f239e9')
+			}
+
 
 			const totalSupplyBefore = await CONTRACT.totalSupply();
 
 			const createRes = await CONTRACT.batchCreate(
-				10,
+				batchSize,
 				to_addresses,
 				token_ids,
 				urls
@@ -137,6 +73,7 @@ describe('ScarceEdition_BatchCreate', function () {
 			);
 		});
 
+		//
 
 		it('batchCreate: revert when batchSize > maxBatch', async () => {
 
